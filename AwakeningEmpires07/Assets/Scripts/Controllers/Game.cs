f33be458d7e1e8d943b24ceb01ff1686dc6d72fb
HVossi92 <hvossi92@gmail.com>
2018-10-25 22:45:46 +0200

    working on save/load functionality. Saving strings out to the harddrive works

---
+++ AwakeningEmpires07/Assets/Scripts/Controllers/Game.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[System.Serializable]
+
+public class Game
+{
+    public string savegameName;
+    public string testString;
+}
